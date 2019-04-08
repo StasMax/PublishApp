3516a9e2bfc137a4e5976d38428e2fa62627e1df
StasMax <maximovich@live.ru>
2019-04-08 11:35:26 +0300

    edit code

--- app/src/main/java/com/example/android/publishapp/presentation/mvp/ui/PublishActivity.java
+++ app/src/main/java/com/example/android/publishapp/presentation/mvp/ui/PublishActivity.java
@@ -1,27 +1,19 @@
 package com.example.android.publishapp.presentation.mvp.ui;
 
-
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.example.android.publishapp.R;
 import com.example.android.publishapp.presentation.adapter.FragmentsViewPagerAdapter;
 import com.example.android.publishapp.presentation.app.App;
-import com.example.android.publishapp.presentation.mvp.ui.fragment.EventFragment;
-import com.example.android.publishapp.presentation.mvp.ui.fragment.PostFragment;
-import com.google.firebase.FirebaseApp;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
 
 public class PublishActivity extends FragmentActivity {
     @BindView(R.id.radioGroup)
